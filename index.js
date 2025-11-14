19b25d8d4de580815f9474a53fbe04c0ad205823
Ghukasyan Anna <ghukasyanna1991@gmail.com>
2025-11-14 16:04:03 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+(async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    });
+
+    const data = await response.json();
+    console.log(data)
+})();
+
